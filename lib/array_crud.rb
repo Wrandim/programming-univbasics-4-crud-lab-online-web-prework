888db10e0b290f16db85a42622b3615d5fda0a5b
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-15 21:01:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,12 +15,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
-  create_an_array.pop
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift 
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
